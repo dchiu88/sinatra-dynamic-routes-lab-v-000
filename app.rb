b8a7f594a96afd7790ec76d7270fd7e7fcf5f96c
David Chiu <davidwchiu@gmail.com>
2018-09-09 19:16:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,17 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  get '/reverse/:name' do
-    name = params[:name]
-    name.reverse
+
+  get '/reversename/:name' do
+    params[:name].reverse
   end
-  
+
   get '/square/:number' do
     num = params[:number]
     (num.to_i**2).to_s
   end
-  
+
   get '/say/:number/:phrase' do
     final_string = ""
     (params[:number].to_i).times do
@@ -19,11 +18,11 @@ class App < Sinatra::Base
     end
     final_string
   end
-  
-   get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
-  
+
   get '/:operation/:number1/:number2' do
     operation = params[:operation]
     number1 = params[:number1].to_i
@@ -41,5 +40,5 @@ class App < Sinatra::Base
     end
     answer.to_s
   end
-  
+
 end
